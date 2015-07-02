7b2a7bee2bd2023ba03ecc91cb2bdc6f5ea51ac4
Jim D <heyjim89@gmail.com>
2015-07-02 11:44:38 -0600

    Finished up Print Object

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
-var removeM = function (str) {
+// REMOVE M
 
-    var output = '';
-    for (var i = 0; i < str.length; i++) {
-        if (str[i] != 'm') {
-            output += str[i];
-        }
-    }
-    return output;
-};
+//var removeM = function (str) {
+//
+//    var output = '';
+//    for (var i = 0; i < str.length; i++) {
+//        if (str[i] != 'm') {
+//            output += str[i];
+//        }
+//    }
+//    return output;
+//};
+
+
+// PRINT OBJECT
+
+
+//var printObject = function (object) {
+//    for (var key in object) {
+//        console.log(key + ' is ' + object[key]);
+//    }
+//}
+
+//var vowels = function (str) {
+//    var output = [];
+//    var map = {
+//        'a': true,
+//        'e': true,
+//        'i': true,
+//        'o': true,
+//        'u': true,
+//        'y': true
+//    };
+//
+//    for (var i = 0; i < str.length; i++) {
+//        if (str[i] in map) {
+//            output.push(str[i]);
+//        }
+//    }
+//    return output;
+//
+//}
+//console.log(vowels('alabama'));
+//
+//console.log(vowels('What evil odd ducks!'));
